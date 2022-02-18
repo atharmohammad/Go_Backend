96d23e2d6bff18ba1daa9eac28e525b614364b5f
atharmohammad <mohd.rule123@gmail.com>
2022-02-18 12:10:40 +0530

    update_delete_api

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 const dbname = "PersonDb"
@@ -46,9 +47,52 @@ func addPerson(res *fiber.Ctx) error {
 	return res.Status(200).Send(response)
 }
 
+func updatePerson(res *fiber.Ctx) error {
+	collection, err := getMongoDbCollection(dbname, collectionName)
+	if err != nil {
+		return res.Status(400).SendString("There is some error! Try again later")
+	}
+	id := res.Params("id")
+	objId, _ := primitive.ObjectIDFromHex(id)
+	var updatePerson Person
+	json.Unmarshal([]byte(res.Body()), &updatePerson)
+	var filter bson.M = bson.M{
+		"_id": objId,
+	}
+	update := bson.M{
+		"$set": updatePerson,
+	}
+	curr, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return res.Status(400).SendString("There is some error! Try again later")
+	}
+	response, _ := json.Marshal(curr)
+	return res.Status(200).Send(response)
+}
+
+func deletePerson(res *fiber.Ctx) error {
+	collection, err := getMongoDbCollection(dbname, collectionName)
+	if err != nil {
+		return res.Status(400).SendString("There is some error! Try again later")
+	}
+	id := res.Params("id")
+	objId, _ := primitive.ObjectIDFromHex(id)
+	var filter bson.M = bson.M{
+		"_id": objId,
+	}
+	curr, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return res.Status(400).SendString("There is some error! Try again later")
+	}
+	response, _ := json.Marshal(curr)
+	return res.Status(200).Send(response)
+}
+
 func main() {
 	app := fiber.New()
 	app.Get("/", indexRoute)
 	app.Post("/create", addPerson)
+	app.Put("/update/:id", updatePerson)
+	app.Delete("/delete/:id", deletePerson)
 	app.Listen(":8000")
 }
